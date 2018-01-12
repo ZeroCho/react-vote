7ac51887e48fbb0f78caa53b8d4bcc07ae98f151
zerocho <zerohch0@gmail.com>
2018-01-13 04:31:41 +0900

    Update: test react16

--- __tests__/render-test.jsx
+++ __tests__/render-test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import expect from 'expect';
-import { shallow } from 'enzyme';
+import { shallow, render, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ReactVote from '../src/ReactVote';
+
+configure({ adapter: new Adapter() });
 
 describe('<ReactVote />', () => {
   it('should render creation-view', () => {
-    const wrapper = shallow(<ReactVote />);
+    const wrapper = render(<ReactVote />);
     expect(wrapper.find('#creation-view').length).toEqual(1);
   });
 
@@ -15,20 +18,24 @@ describe('<ReactVote />', () => {
   });
 
   it('should render vote-view', () => {
-    const wrapper = shallow(<ReactVote data={{
-      title: 'Ongoing Vote with Custom Text',
-      items: [{ title: 'a', count: 5 }, { title: 'b', count: 3 }],
-      closed: false,
-    }} />);
+    const wrapper = shallow(<ReactVote
+      data={{
+        title: 'Ongoing Vote with Custom Text',
+        items: [{ title: 'a', count: 5 }, { title: 'b', count: 3 }],
+        closed: false,
+      }}
+    />);
     expect(wrapper.find('#vote-view').length).toEqual(1);
   });
 
   it('should not render creation-view', () => {
-    const wrapper = shallow(<ReactVote data={{
-      title: 'Ongoing Vote with Custom Text',
-      items: [{ title: 'a', count: 5 }, { title: 'b', count: 3 }],
-      closed: false,
-    }} />);
+    const wrapper = shallow(<ReactVote
+      data={{
+        title: 'Ongoing Vote with Custom Text',
+        items: [{ title: 'a', count: 5 }, { title: 'b', count: 3 }],
+        closed: false,
+      }}
+    />);
     expect(wrapper.find('#creation-view').length).toEqual(0);
   });
 });
